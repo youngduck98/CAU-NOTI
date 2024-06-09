2bdc0d6970d45fe92d672d86d59736b829225717
KYD <97766222+youngduck98@users.noreply.github.com>
2024-06-09 15:34:00 +0900

    [#7 feat] add getter, builder, tostring at Annotaion because: need to add build and get, toString  function at Annotaion object for JsoupEngine and testing, result: add getter, builder, tostring functions at Annotaion

--- src/main/java/org/example/caunoti/domain/object/Annotation/Annotation.java
+++ src/main/java/org/example/caunoti/domain/object/Annotation/Annotation.java
@@ -1,7 +1,14 @@
 package org.example.caunoti.domain.object.Annotation;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.time.LocalDate;
 
+@Getter
+@Builder
+@ToString
 public class Annotation {
     int id;
     int postNumber;

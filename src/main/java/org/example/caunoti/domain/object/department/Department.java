656338159e9f480d6ac96bdee9e20b8928e1daa8
KYD <97766222+youngduck98@users.noreply.github.com>
2024-06-09 14:12:41 +0900

    [object] add department domain object add department domain object On branch domain/Object/#5 Changes to be committed: 	new file:   src/main/java/org/example/caunoti/domain/object/Site/Site.java 	new file:   src/main/java/org/example/caunoti/domain/object/department/Department.java

---
+++ src/main/java/org/example/caunoti/domain/object/department/Department.java
@@ -0,0 +1,12 @@
+package org.example.caunoti.domain.object.department;
+
+import org.example.caunoti.domain.object.Site.Site;
+
+import java.util.List;
+
+public class Department {
+    int id;
+    String koName;
+    String enName;
+    List<Site> relateSites;
+}

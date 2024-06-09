c042a96c5aba7945168e2e3ab430f2b0294290cd
KYD <97766222+youngduck98@users.noreply.github.com>
2024-06-09 16:57:55 +0900

    temporary commit for Link refactor

--- src/main/java/org/example/caunoti/domain/object/crawlingEngine/CrawlingEngine.java
+++ src/main/java/org/example/caunoti/domain/object/crawlingEngine/CrawlingEngine.java
@@ -1,7 +1,7 @@
 package org.example.caunoti.domain.object.crawlingEngine;
 
 import org.example.caunoti.domain.object.Site.Site;
-import org.example.caunoti.domain.object.Annotation.Annotation;
+import org.example.caunoti.domain.object.Announcement.Announcement;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,5 +11,5 @@ public interface CrawlingEngine {
     String https = "https";
     int maxTry = 3;
     //해당 사이트에서 postMum 부터 하나씩 올라가며 숫자가 없을 때 까지 크롤링한다.
-    List<Annotation> crawlFrom(Site site, Integer postNum) throws IOException;
+    List<Announcement> crawlFrom(Site site, Integer postNum) throws IOException;
 }

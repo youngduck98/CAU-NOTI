33c32578c3f0a5c493dfb563bf5982c31e2f963e
KYD <97766222+youngduck98@users.noreply.github.com>
2024-06-09 16:25:45 +0900

    [feat] add maxTry feature to CrawlingEngine because: need to see easy how many we can try when crawling site result: can see maxTry at interface

--- src/main/java/org/example/caunoti/domain/object/crawlingEngine/CrawlingEngine.java
+++ src/main/java/org/example/caunoti/domain/object/crawlingEngine/CrawlingEngine.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface CrawlingEngine {
     String http = "http";
     String https = "https";
+    int maxTry = 3;
     //해당 사이트에서 postMum 부터 하나씩 올라가며 숫자가 없을 때 까지 크롤링한다.
     List<Annotation> crawlFrom(Site site, Integer postNum) throws IOException;
 }

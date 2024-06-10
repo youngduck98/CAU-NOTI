ff8110f637bbb54cad98e308677af1f4c7206f7e
KYD <97766222+youngduck98@users.noreply.github.com>
2024-06-11 00:05:55 +0900

    [object] add two static final variable reason: crawling engine need to work with no error in this two condition 1. when fail to crawling because of temporary jsoup error 2. when postnum is not continuous changed add two feature which meaning that max try about one AnnounceMent and max see to check it is really end or just not continuous

--- src/main/java/org/example/caunoti/domain/object/crawlingEngine/CrawlingEngine.java
+++ src/main/java/org/example/caunoti/domain/object/crawlingEngine/CrawlingEngine.java
@@ -6,12 +6,14 @@ import org.example.caunoti.domain.object.Site.Site;
 import org.example.caunoti.domain.object.Announcement.Announcement;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 public interface CrawlingEngine {
     String http = "http";
     String https = "https";
-    int maxTry = 3;
-    //해당 사이트에서 postMum 부터 하나씩 올라가며 숫자가 없을 때 까지 크롤링한다.
-    List<Announcement> crawlFrom(Site site, Integer postNum) throws IOException;
+    int maxTryAtOneAnnouncement = 3;
+    int maxSeeAfterFindOmitNumber = 3;
+    //해당 사이트의 lastPostNum +1번 uid 부터 하나씩 올라가며 숫자가 없을 때 까지 크롤링한다.
+    List<Announcement> crawlFrom(Site site) throws IOException, URISyntaxException;
 }

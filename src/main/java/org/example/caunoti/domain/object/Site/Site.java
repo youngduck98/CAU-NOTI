e78dfd0e15c3eab100b4b963dd9fac2fe55f9eb5
KYD <97766222+youngduck98@users.noreply.github.com>
2024-06-10 19:03:41 +0900

    [feat] add lastPostNum feature to Site reason it will be more easy to find site's last postnum when it's data in own's object

--- src/main/java/org/example/caunoti/domain/object/Site/Site.java
+++ src/main/java/org/example/caunoti/domain/object/Site/Site.java
@@ -1,15 +1,20 @@
 package org.example.caunoti.domain.object.Site;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.net.URISyntaxException;
 
 @Getter
+@Builder
 public class Site{
     int id;
     Link link;
     String koName;
     String enName;
+
+    int lastPostNum;
     boolean crawlAble;
 
     public Link getLink() throws URISyntaxException {
